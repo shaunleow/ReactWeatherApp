0936a11222cd7ee118b1124f534540f34daa9c25
shaunleow <shaunleow@gmail.com>
2017-06-19 16:25:00 +0800

    About page semicolon remove

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = props => {
   return (
     <div>
-      <h2>About Component</h2>;
+      <h2>About Component</h2>
       <p>Did you find the weather? Great! Simple right?</p>
     </div>
   );
