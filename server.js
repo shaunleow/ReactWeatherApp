46fd442d1e9b5fa4ad8d182930356b988da33247
shaunleow <shaunleow@gmail.com>
2017-06-19 15:25:17 +0800

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-// node’s keyword, require, to load in the module. argument is string name of the module
-// now can access entire express api
 var express = require('express');
 
-// call express library as a function. nothing as arguments.
-// tell which folder we want to serve.
-// app.use lets you add functionality to your express app
-// express.static is going to specify a folder name we want exposed to the web server
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http://') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 app.use(express.static('public'));
 
-// start the server
-app.listen(3000, function(){
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function() {
+  console.log('Express server is up on port ' + PORT);
 });
