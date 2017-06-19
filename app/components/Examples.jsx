42ad412e861c6ef9305be6b2fe63b6aa8e1b9b0d
shaunleow <shaunleow@gmail.com>
2017-06-19 14:46:38 +0800

    Tweaked examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,12 @@
 var React = require('react');
 
-var Examples = React.createClass ({
-  render: function (){
-    return (
+var Examples = props => {
+  return (
+    <div>
       <h2>Examples Component</h2>
-    );
-  }
-});
+      <p>This is an examples page</p>
+    </div>
+  );
+};
 
 module.exports = Examples;
