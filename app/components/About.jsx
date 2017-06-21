3854eaf7b36f15802b9ee3db1b79946e8975958e
shaunleow <shaunleow@gmail.com>
2017-06-21 20:09:43 +0800

    About page updated

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,24 @@
-var React = require('react');
+var React = require("react");
 
 var About = props => {
   return (
     <div>
-      <h2>About Component</h2>
-      <p>Did you find the weather? Great! Simple right?</p>
+      <h1 className="text-center">About</h1>
+      <p className="text-center">
+        Did you find the weather? Great! Simple right? It's magical and is
+        accurate by TWO decimal places! EXPELLIARMUS!
+      </p>
+      <h5>This was built with:</h5>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a>: JavaScript
+          framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a>: was
+          responsible for finding you the weather!
+        </li>
+      </ul>
     </div>
   );
 };
